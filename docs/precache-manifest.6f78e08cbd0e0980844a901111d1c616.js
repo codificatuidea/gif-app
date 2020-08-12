024fbba1fabbcd6836525ac4da56d97220d348fd
Sebastian Rincon <sebastian@meltstudio.co>
2020-08-11 21:00:29 -0500

    Se agrega docs

---
+++ docs/precache-manifest.6f78e08cbd0e0980844a901111d1c616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb88cd8c39aa08c6a4ea2a258e65a5df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af74aaa26f26f3cf28d",
+    "url": "/static/css/main.cb0044e3.chunk.css"
+  },
+  {
+    "revision": "6b1578778fd38a87f0cb",
+    "url": "/static/js/2.a685c4d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a685c4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af74aaa26f26f3cf28d",
+    "url": "/static/js/main.25e0e53a.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
